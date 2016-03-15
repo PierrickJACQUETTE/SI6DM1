e2fea44e3c10de0c0efe05d875c2e6b016c5db22
PierrickJACQUETTE <pierrick.jacquette@gmail.com>
2016-03-15 22:58:08 +0100

    chiffre/dechiffredeVigenere

--- src/securiteL3/Permutation.java
+++ src/securiteL3/Permutation.java
@@ -1,15 +1,18 @@
-//package securiteL3;
-//
-//public class Permutation<E> implements Transcrire<E> {
-//
-//	@Override
-//	public E chiffrer(E e) {
-//		return null;
-//	}
-//
-//	@Override
-//	public E dechiffrer(E e) {
-//		return null;
-//	}
-//
-//}
+package securiteL3;
+
+public class Permutation implements Transcrire{
+
+	@Override
+	public StringBuilder chiffrer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StringBuilder dechiffrer(String decalage, StringBuilder strp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
--- src/securiteL3/Vigenere.java
+++ src/securiteL3/Vigenere.java
@@ -1,16 +1,71 @@
-//package securiteL3;
-//
-//public class Vigenere<E> implements Transcrire<E> {
-//
-//	@Override
-//	public E chiffrer(E e) {
-//		return null;
-//	}
-//
-//	@Override
-//	public E dechiffrer(E e) {
-//		return null;
-//	}
-//
-//
-//}
+package securiteL3;
+
+public class Vigenere implements Transcrire {
+
+	StringBuilder mot;
+	StringBuilder motCle;
+	
+	public Vigenere(StringBuilder mot, StringBuilder motCle) {
+		this.mot = mot;
+		this.motCle = motCle;
+	}
+	
+	@Override
+	public StringBuilder chiffrer() {
+		StringBuilder resultat = new StringBuilder();
+		char lettre = 0;
+		char lettreCle = 0;
+		for (int i = 0; i < this.mot.length(); i++) {
+			for (int j = 0; j < this.motCle.length(); j++) {
+				if (i < this.mot.length()) {
+					lettre = this.mot.charAt(i);
+					lettreCle = this.motCle.charAt(j);
+					if (lettre + lettreCle > 'a' + 'z') {
+						resultat.append((char)(lettre + lettreCle - 'z'));
+					} else if (lettre + lettreCle < 'a') {
+						resultat.append((char)(lettre + lettreCle + 'a'));
+					} else {
+						resultat.append((char)(lettre + lettreCle - 'a'));
+					}
+					if (j < this.motCle.length() - 1) {
+						i++;
+					}
+				}
+			}
+		}
+		return resultat;
+	}
+
+	@Override
+	public StringBuilder dechiffrer(String decalage, StringBuilder strp) {
+		StringBuilder resultat = new StringBuilder();
+		char lettre = 0;
+		char lettreCle = 0;
+		for (int i = 0; i < this.mot.length(); i++) {
+			for (int j = 0; j < this.motCle.length(); j++) {
+				if (i < this.mot.length()) {
+
+					lettre = this.mot.charAt(i);
+
+					lettreCle = this.motCle.charAt(j);
+
+					if (lettre - lettreCle <= 0) {
+						resultat.append((char) (lettre - lettreCle + 'z'));
+					} else if (lettre - lettreCle < 'a') {
+						resultat.append((char)(lettre - lettreCle + 'a'));
+					} else {
+						resultat.append((char)(lettre + lettreCle - 'a'));
+					}
+					if (j < this.motCle.length() - 1) {
+						i++;
+					}
+				}
+			}
+		}
+		return resultat;
+	}
+
+
+
+
+}
